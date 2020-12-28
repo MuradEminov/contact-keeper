0dc39cff0e54b032588cd2287b8b357deee2e39a
MuradEminov <murademinov88@gmail.com>
2020-12-28 17:46:24 +0400

    Backend Routes created

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
-
 const app = express();
 
-app.get("/", (req, res) => res.send({ msg: "Welcome to Contact Keeper API" }));
+app.get("/", (req, res) => res.json({ msg: "Welcome to Contact Keeper API" }));
+
+//Define Routes
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
