06ff61c9180f949c83d6e523b55359c113b6f11d
MuradEminov <murademinov88@gmail.com>
2021-01-21 14:02:50 +0400

    check body parameter values with Express-Validator  added for Apple route

--- routes/apple.js
+++ routes/apple.js
@@ -1,22 +1,32 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 const Apple = require("../models/Apple");
 
-router.post("/", async (req, res) => {
-  try {
-    const { name } = req.body;
-    let apple = new Apple({
-      name,
-    });
+router.post(
+  "/",
+  [body("name", "Name must be minimum 8 characters").isLength({ min: 8 })],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    await apple.save();
-    console.log("Apple saved.");
-    res.send("Apple saved");
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send("Server Error");
+    try {
+      const { name } = req.body;
+      let apple = new Apple({
+        name,
+      });
+
+      await apple.save();
+      console.log("Apple saved.");
+      res.send("Apple saved");
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Server Error");
+    }
   }
-});
+);
 
 module.exports = router;
